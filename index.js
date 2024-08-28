5abe248f7a0ca5b3633a1cab375eea051173f2ee
tharushadinuth(pro) <gunasekarawtd.21@uom.lk>
2024-08-28 14:50:06 +0530

    build mobile app

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
 app.use("/api/user/", UserRoutes);
+
+
 // error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
@@ -23,12 +25,16 @@ app.use((err, req, res, next) => {
   });
 });
 
+
 app.get("/", async (req, res) => {
   res.status(200).json({
     message: "Hello developers from GFG",
   });
 });
+
 var mongoURL = 'mongodb+srv://dinukgunasekara286:Bzt3SrJYtm5htN4H@fitnessdb.w1xusgy.mongodb.net/'
+
+
 const connectDB = () => {
   mongoose.set("strictQuery", true);
   mongoose
@@ -40,13 +46,16 @@ const connectDB = () => {
     });
 };
 
+
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(8010, () => console.log("Server started on port 8010"));
   } catch (error) {
     console.log(error);
   }
 };
+
+
 
 startServer();
