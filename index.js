546ffa3dc18191ca8c5f0c42dfd27ab8665f6339
tharushadinuth <112651385+Tharusha-2000@users.noreply.github.com>
2024-09-11 23:33:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://fitness-track-frontend-using-mern-zjgw.vercel.app'  // Allow requests from your frontend
-}));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
