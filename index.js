5cb50845cd0878b02af9e00f03ff6617c5c79ba5
tharushadinuth <112651385+Tharusha-2000@users.noreply.github.com>
2024-09-11 23:12:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://fitness-track-frontend-using-mern-zjgw.vercel.app/'  // Allow requests from your frontend
+  origin: 'https://fitness-track-frontend-using-mern-zjgw.vercel.app'  // Allow requests from your frontend
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
